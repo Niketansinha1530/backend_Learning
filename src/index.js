b0819e8027602abf8b05c1f11e78ba4bf8facdb0
Niketansinha1530 <niketansinha1@gmail.com>
2024-04-13 10:18:44 +0530

    db error handing

--- src/index.js
+++ src/index.js
@@ -10,9 +10,21 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error: ",error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed",err);
+})
 
-
+//Cors fullform is Cross origin resourse sharing
 
 
 
